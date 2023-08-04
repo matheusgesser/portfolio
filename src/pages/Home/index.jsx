9ed4561bce4a6a69eb8772b23c511690d4aaaafa
MatheusGesser <matheusmvg@hotmail.com>
2023-08-04 14:44:41 -0300

    feat: add email button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,10 @@ import JSLogo from '../../assets/home/js.png'
 import ReactLogo from '../../assets/home/react.png'
 import SassLogo from '../../assets/home/sass.png'
 import { useAnimations } from './useAnimations'
+import { RxCopy } from 'react-icons/rx'
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+
 
 export function Home({ theme, t }) {
   useAnimations()
@@ -15,10 +19,22 @@ export function Home({ theme, t }) {
     })
   }
 
+  function copyMail(event) {
+    navigator.clipboard.writeText('matheusmvg@hotmail.com')
+    setTimeout(() => {
+      event.target._tippy.hide()
+    }, 3000);
+  }
+
   return (
     <main id='home' className={theme} onMouseMove={IconMove}>
-      <h1 data-speedx={12} data-speedy={-6}>Matheus Gesser</h1>
-      <h2 data-speedx={16} data-speedy={-8}>{t('position')}</h2>
+      <h1 data-speedx={20} data-speedy={0}>Matheus Gesser</h1>
+      <h2 data-speedx={15} data-speedy={0}>{t('position')}</h2>
+      <Tippy trigger='click' placement='bottom' content={t('copy')} className={theme}>
+        <button data-speedx={30} data-speedy={0} onClick={copyMail}>
+          <RxCopy />matheusmvg@hotmail.com
+        </button>
+      </Tippy>
         <img src={JSLogo} id='js' data-speedx={-30} data-speedy={-50} alt="JavaScript Logo" />
         <img src={ReactLogo} id='react' data-speedx={60} data-speedy={-40} alt="React Logo" />
         <img src={SassLogo} id='sass' data-speedx={-50} data-speedy={60} alt="Sass Logo" />
