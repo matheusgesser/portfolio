cb5f11611120e959132ea446f21184686d91beb0
MatheusGesser <matheusmvg@hotmail.com>
2023-08-16 19:04:51 -0300

    feat: add projects initial idea

---
+++ src/assets/data/projects.js
@@ -0,0 +1,27 @@
+import TalkAi from '../../assets/projects/talkai.png'
+import ReciclaIdeias from '../../assets/projects/reciclaideias.jpg'
+
+export const ProjectsData = [
+  {
+    "id": 1,
+    "image": TalkAi,
+    "title": "TalkAI",
+    "description": "talk-ai",
+    "labels": [
+      "React",
+      "Styled Components",
+      "AWS"
+    ]
+  },
+  {
+    "id": 2,
+    "image": ReciclaIdeias,
+    "title": "ReciclaIdeias",
+    "description": "recicla-ideias",
+    "labels": [
+      "React",
+      "Sass",
+      "React-Router"
+    ]
+  }
+]
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,12 +1,65 @@
-import './styles.scss'
+import { ProjectsData } from '../../assets/data/projects'
+import { useRef, useState } from 'react';
 // import { useAnimations } from './useAnimations'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import './styles.scss'
+
+import { CgWebsite } from 'react-icons/cg'
+import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md'
+import Laptop from '../../assets/projects/laptop.png'
 
 export function Projects({ theme, t }) {
-  // useAnimations()
+  const swiper = useRef(null);
+  const [projectShown, setProjectShown] = useState(0)
+
+  function handleChange(e) {
+    setProjectShown(e.realIndex)
+  }
+  
+  function handlePrevious() {
+    swiper.current.swiper.slidePrev()
+  }
+
+  function handleNext() {
+    swiper.current.swiper.slideNext()
+  }
 
   return (
-    <main id='projects' className={theme}>
-      <h1>Projects {t('resume')}</h1>
-    </main>
+    <article id='projects' className={theme}>
+      <section className="post">
+        <button className="prev-btn" onClick={handlePrevious}>
+          <MdArrowBackIosNew />
+        </button>
+        <Swiper loop grabCursor ref={swiper} onSlideChange={handleChange}>
+          {ProjectsData.map(project => (
+            <SwiperSlide key={project.id}>
+              <img src={project.image} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button className="next-btn" onClick={handleNext}>
+          <MdArrowForwardIos />
+        </button>
+        <img className="laptop-overlay" src={Laptop}>
+          
+        </img>
+      </section>
+      <section className="info">
+        <section className="labels">
+          {ProjectsData[projectShown].labels.map(label => <i key={label}>{label}</i>)}
+        </section>
+        <h2>
+          {ProjectsData[projectShown].title}
+        </h2>
+        <p>
+          {t(ProjectsData[projectShown].description)}
+        </p>
+        <section className="buttons">
+          <button>Demo <CgWebsite /></button>
+          <button>GitHub</button>
+        </section>
+      </section>
+    </article>
   )
 }
