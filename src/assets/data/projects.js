bc96bce450153d7c68134d2ba6f31e56ca8a151b
MatheusGesser <matheusmvg@hotmail.com>
2023-08-21 21:47:37 -0300

    feat: add more projects to 'database'

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,5 +1,7 @@
 import TalkAi from '../../assets/projects/talkai.png'
 import ReciclaIdeias from '../../assets/projects/reciclaideias.jpg'
+import GitCombat from '../../assets/projects/gitcombat.png'
+import TechTenzies from '../../assets/projects/techtenzies.jpg'
 
 export const ProjectsData = [
   {
@@ -9,19 +11,55 @@ export const ProjectsData = [
     "description": "talk-ai",
     "labels": [
       "React",
+      "NodeJS",
+      "OpenAI API",
       "Styled Components",
       "AWS"
-    ]
+    ],
+    "deploylink" : 'https://talkai.netlify.app/',
+    "githublink" : 'https://github.com/matheusgesser/talk-ai/'
   },
   {
     "id": 2,
+    "image": GitCombat,
+    "title": "GitCombat",
+    "description": "git-combat",
+    "labels": [
+      "React",
+      "TypeScript",
+      "Styled Components",
+      "Octokit (GitHub API)"
+    ],
+    "deploylink" : 'https://gitcombat.netlify.app/',
+    "githublink" : 'https://github.com/matheusgesser/git-combat/'
+  },
+  {
+    "id": 3,
+    "image": TechTenzies,
+    "title": "TechTenzies",
+    "description": "tech-tenzies",
+    "labels": [
+      "React",
+      "Styled Components",
+      "Design",
+      "User Experience",
+    ],
+    "deploylink" : 'https://techtenzies.netlify.app/',
+    "githublink" : 'https://github.com/matheusgesser/tech-tenzies/'
+  },
+  {
+    "id": 4,
     "image": ReciclaIdeias,
     "title": "ReciclaIdeias",
     "description": "recicla-ideias",
     "labels": [
       "React",
       "Sass",
-      "React-Router"
-    ]
+      "React-Router",
+      "VLibras",
+      "EmailJS"
+    ],
+    "deploylink" : 'https://reciclaideias.netlify.app/',
+    "githublink" : 'https://github.com/matheusgesser/recicla-ideias/'
   }
 ]
