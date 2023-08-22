e62770d6ee6981406c26714e927dca5d236a19d7
MatheusGesser <matheusmvg@hotmail.com>
2023-08-22 11:32:51 -0300

    feat: add more projects to 'database'

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -2,6 +2,9 @@ import TalkAi from '../../assets/projects/talkai.png'
 import ReciclaIdeias from '../../assets/projects/reciclaideias.jpg'
 import GitCombat from '../../assets/projects/gitcombat.png'
 import TechTenzies from '../../assets/projects/techtenzies.jpg'
+import GesserBank from '../../assets/projects/gesserbank.jpg'
+import Portfolio from '../../assets/projects/portfolio.jpg'
+import GuessNationality from '../../assets/projects/guessnationality.jpg'
 
 export const ProjectsData = [
   {
@@ -61,5 +64,48 @@ export const ProjectsData = [
     ],
     "deploylink" : 'https://reciclaideias.netlify.app/',
     "githublink" : 'https://github.com/matheusgesser/recicla-ideias/'
+  },
+  {
+    "id": 5,
+    "image": GesserBank,
+    "title": "GesserBank",
+    "description": "gesser-bank",
+    "labels": [
+      "JavaScript Vanilla",
+      "Styled Components",
+      "GSAP"
+    ],
+    "deploylink" : 'https://gesserbank.netlify.app/',
+    "githublink" : 'https://github.com/matheusgesser/gesser-bank/'
+  },
+  {
+    "id": 6,
+    "image": Portfolio,
+    "title": "Portfolio",
+    "description": "portfolio",
+    "labels": [
+      "React",
+      "Sass",
+      "GSAP",
+      "Swiper",
+      "EmailJS",
+      "Tippy.js"
+    ],
+    "deploylink" : '#',
+    "githublink" : 'https://github.com/matheusgesser/portfolio/'
+  },
+  {
+    "id": 7,
+    "image": GuessNationality,
+    "title": "GuessNationality",
+    "description": "guess-nationality",
+    "labels": [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Vanilla",
+    ],
+    "deploylink" : 'https://namepredict.netlify.app/',
+    "githublink" : 'https://github.com/matheusgesser/guess-nationality/'
   }
 ]
