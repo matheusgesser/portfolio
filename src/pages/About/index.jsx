bd95535baaad9f4db3a8c823748175cd36866e81
MatheusGesser <matheusmvg@hotmail.com>
2023-09-21 20:06:52 -0300

    style: new about section design

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,31 +1,45 @@
-import './styles.scss'
-import ProfilePic from '../../assets/profile.jpg'
-import { RxDownload } from 'react-icons/rx'
+import "./styles.scss";
+import ProfilePic from "../../assets/profile.jpg";
+import { RxDownload } from "react-icons/rx";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import { skills } from "./skills";
 
 export function About({ theme, t }) {
-
   return (
-    <article id='about' className={theme}>
-      <section className='profile'>
-        <img src={ProfilePic} alt='Matheus front picture' />
-          <section className='content'>
-            <p><span>❝</span>{t('aboutme')}<span>❞</span></p>
-            <a href={t('pdf')} target='_blank' rel='noreferrer'>
-              <RxDownload />{t('resume')}
-            </a>
-          </section>
+    <article id="about" className={theme}>
+      <section className="profile">
+        <img src={ProfilePic} alt="Matheus front picture" />
+        <a href={t("pdf")} target="_blank" rel="noreferrer">
+          <RxDownload />
+          {t("resume")}
+        </a>
+        <section className="skills">
+          {skills.map(({ label, icon, major }) => (
+            <Tippy
+              key={label}
+              placement="top"
+              content={label}
+              className={theme}
+            >
+              <img src={icon} className={major && "major"} />
+            </Tippy>
+          ))}
+        </section>
       </section>
-      <section className='skills'>
-        <h2 className='major'>React</h2>
-        <h2 className='major'>JavaScript</h2>
-        <h2>TypeScript</h2>
-        <h2>Git</h2>
-        <h2>HTML</h2>
-        <h2 className='major'>CSS</h2>
-        <h2>Sass</h2>
-        <h2>NodeJS</h2>
-        <h2>SQL</h2>
+      <section className="content">
+        {t("lang") == "br" ? (
+          <p>
+            Transformando código em <del data-bg="blue">páginas</del>{" "}
+            <ins>experiências digitais marcantes</ins>
+          </p>
+        ) : (
+          <p>
+            Turning code into <del data-bg="blue">pages</del>{" "}
+            <ins>outstanding digital experiences</ins>
+          </p>
+        )}
       </section>
     </article>
-  )
+  );
 }
---
+++ src/pages/About/skills.js
@@ -0,0 +1,51 @@
+export const skills = [
+  {
+    label: "React",
+    icon: "https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png",
+    major: true,
+  },
+  {
+    label: "JavaScript",
+    icon: "https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png",
+    major: true,
+  },
+  {
+    label: "TypeScript",
+    icon: "https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png",
+  },
+  {
+    label: "HTML5",
+    icon: "https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png",
+    major: true,
+  },
+  {
+    label: "CSS3",
+    icon: "https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png",
+    major: true,
+  },
+  {
+    label: "Sass",
+    icon: "https://user-images.githubusercontent.com/25181517/192158956-48192682-23d5-4bfc-9dfb-6511ade346bc.png",
+    major: true,
+  },
+  {
+    label: "Node.js",
+    icon: "https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png",
+  },
+  {
+    label: "Express",
+    icon: "https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png",
+  },
+  {
+    label: "Next.js",
+    icon: "https://github.com/marwin1991/profile-technology-icons/assets/136815194/5f8c622c-c217-4649-b0a9-7e0ee24bd704",
+  },
+  {
+    label: "Tailwind CSS",
+    icon: "https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png",
+  },
+  {
+    label: "Cypress",
+    icon: "https://user-images.githubusercontent.com/68279555/200387386-276c709f-380b-46cc-81fd-f292985927a8.png",
+  },
+];
